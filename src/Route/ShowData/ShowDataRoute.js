cebc1336e338366dc8b3793e3350d8c72c46d7ee
hindustantech <kumarrupees507@gmail.com>
2025-08-19 18:44:10 +0530

    'add'

--- src/Route/ShowData/ShowDataRoute.js
+++ src/Route/ShowData/ShowDataRoute.js
@@ -4,7 +4,9 @@ import { getAvailableCandidates,getJobPostings } from "../../Controller/ShowData
 const router = express.Router();
 
 // GET /api/jobs?page=1&limit=10
-router.get("/getcandidates", getJobPostings);
-router.get("/getjob", getAvailableCandidates);
+router.get("/getcandidates", getAvailableCandidates );
+router.get("/getjob",getJobPostings );
 
 export default router;
+
+
