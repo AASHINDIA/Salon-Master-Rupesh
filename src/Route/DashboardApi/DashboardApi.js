de61f419e0bd007b3e9cc4e9664037151dbf34dd
hindustantech <kumarrupees507@gmail.com>
2025-08-28 17:47:30 +0530

    add

--- src/Route/DashboardApi/DashboardApi.js
+++ src/Route/DashboardApi/DashboardApi.js
@@ -1,8 +1,10 @@
 import { CountsoftheUsers, getDashboardStats } from "../../Controller/DasboardApis/DashboardApi.js";
+import { protect } from '../../Middlewares/authMiddleware/auth.js';
 import express from 'express';
+
 const router = express.Router();
 // Dashboard API route
-router.get('/counts', CountsoftheUsers);
+router.get('/counts',protect, CountsoftheUsers);
 
 // Add more dashboard-related routes here as needed
 router.get('/getDashboardStats', getDashboardStats);
