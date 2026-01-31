f74ca6aba42a7bb358a6c1bba4f66bf43c759799
hindustantech <kumarrupees505@gmail.com>
2026-01-31 10:39:42 +0530

    remove the active ture

--- src/Route/ShowData/ShowDataRoute.js
+++ src/Route/ShowData/ShowDataRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getAvailableCandidates,getJobPostings } from "../../Controller/ShowData/getCandidate.js";
 
-const router = express.Router();
+const   router = express.Router();
 
 // GET /api/jobs?page=1&limit=10
 router.get("/getcandidates", getAvailableCandidates );
