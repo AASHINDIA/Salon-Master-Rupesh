dfc0119480a4971896246061a6ff96eecac8034e
hindustantech <kumarrupees505@gmail.com>
2026-03-03 13:43:21 +0530

    add

--- src/Controller/Tranginvideo/payment.js
+++ src/Controller/Tranginvideo/payment.js
@@ -6,8 +6,8 @@ import mongoose from "mongoose";
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
 
