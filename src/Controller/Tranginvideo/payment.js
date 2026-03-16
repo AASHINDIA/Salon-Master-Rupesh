634e6a632088afe5cdc9fd09cba734f690ce2615
hindustantech <kumarrupees505@gmail.com>
2026-03-16 15:44:53 +0530

    add

--- src/Controller/Tranginvideo/payment.js
+++ src/Controller/Tranginvideo/payment.js
@@ -5,6 +5,10 @@ import TrainingPurchase from "../../Modal/SuperAdmin/BuyTraning.js";
 import mongoose from "mongoose";
 import Razorpay from "razorpay";
 import crypto from "crypto";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET,
